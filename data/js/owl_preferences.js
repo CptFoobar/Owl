5d77847d654d2671c706cd1d0db40fc35b0bf05d
Alan-Solitar <alsolita1@gmail.com>
2018-04-21 01:13:22 -0400

    export implemented, import still in progress, button designs need to be improved

--- data/js/owl_preferences.js
+++ data/js/owl_preferences.js
@@ -51,6 +51,73 @@
                 url: "configure_sites.html"
             });
         });
+        const saveDataToFile = function(data, fileName, properties) {
+            window.URL = window.URL || window.webkitURL;
+            var file = new File(data, fileName, properties),
+              link = document.createElement('a');
+            link.href = window.URL.createObjectURL(file);
+            link.download = fileName;
+            document.body.appendChild(link);
+            link.click();
+            window.URL.revokeObjectURL(link.href);// possible problem here
+            document.body.removeChild(link)
+          };
+        //create on click listener for export setting button
+        $("#exportSettings").click(function() {
+        
+            browser.storage.local.get().then((settings) => {
+                // reset storage if nothing is stored so far
+                if (!settings || (Object.keys(settings).length === 0 && settings.constructor === Object)) {
+                    resetStorage();
+                } else {
+                    const fileName = 'owl_settings.json',
+                    properties = {
+                    type: 'octet/stream'
+                    },
+                    data = [JSON.stringify({
+                        whiteSites:settings.whiteSites,
+                        alwaysEnableSites:settings.alwaysEnableSites,
+                        classicSiteList:settings.classicSiteList
+                    })];
+                    saveDataToFile(data, fileName, properties);
+                    console.log('save')
+                }
+                
+            }, (err)=>{
+                console.log(err)
+            });
+        
+            
+        });
+
+        $("#importSettings").change(function(event){
+            const filesList = event.target.files;
+            //console.log('print this',filesList[0])
+            if(filesList.length===1){
+                const file = filesList[0]
+                console.log(file)
+                readJSON(file)
+
+            }
+        })
+
+
+        function readJSON(file) {
+            const reader = new FileReader();      
+            reader.onload = function(e) {
+              let text = JSON.parse(e.target.result);
+              console.log(text.whiteSites)
+            }
+            reader.onerror = function(err) {
+              console.log(
+                  err, 
+                  err.loaded, 
+                  err.loaded === 0, 
+                  file);
+            }
+  
+            reader.readAsText(file);
+        }
 
     });
 }());
